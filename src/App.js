15b3ebb9635525ca7a11b17939c3d77fee633d85
Adamska <adams.roy27@gmail.com>
2017-04-26 10:57:40 +0200

    component updatephotoprofil, rajout modal(en cours)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import Form from './components/Form/Form.jsx';
 import NavbarInstance from './components/HeaderPublic/Navbar.jsx';
-
+import ProfilPhoto from './components/ProfilPhoto/ProfilPhoto.jsx';
+import logo from '../public/logo-couleur.png';
+import FormUpdateProfilPhoto from './components/Form/FormUpdateProfilPhoto.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: {display: 'none'},
+    }
+  }
+
+  openForm = (event) => {
+    this.setState({
+      display: {display: 'block'}
+    });
+  }
+
+  closeForm = () => {
+    this.setState({
+      display: {display: 'none'}
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <NavbarInstance />
-        <Form />
+
+        <ProfilPhoto source={logo} alt="logo" onclick={this.openForm} />
+
+        <FormUpdateProfilPhoto  display={this.state.display} onclick={this.closeForm}/>
       </div>
     );
   }
